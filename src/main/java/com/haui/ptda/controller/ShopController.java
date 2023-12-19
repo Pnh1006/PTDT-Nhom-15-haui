7212b25d5fa63b6f834d21d287a9f379eac014cc
linhne1709 <linhthobinha@gmail.com>
2023-12-19 20:23:25 +0700

    implement shop page

--- src/main/java/com/haui/ptda/controller/ShopController.java
+++ src/main/java/com/haui/ptda/controller/ShopController.java
@@ -1,14 +1,26 @@
 package com.haui.ptda.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShopController {
 
     @GetMapping("/home")
     public String goToShop(){
-        return "index";
+        return "home";
+    }
+
+    @GetMapping("/shop")
+    public String getProducts(){
+        return "shop";
+    }
+
+    @GetMapping("/product")
+    public String getProductDetail(){
+        return "product-detail";
     }
 
 }
