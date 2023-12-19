142d45fef79e9d6bb3f9fb7fe0903bd5b7bf710c
Pnh1006 <pnh1006.pnh@gmail.com>
2023-12-19 19:34:03 +0700

    init - project

---
+++ src/main/java/com/haui/ptda/PtdaNhom15Application.java
@@ -0,0 +1,13 @@
+package com.haui.ptda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtdaNhom15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtdaNhom15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/haui/ptda/PtdaNhom15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haui.ptda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtdaNhom15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
